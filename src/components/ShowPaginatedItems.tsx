6aad52bfed5ebb66ba51ac4d1526d78c0d5d8c1a
Ilemar <ilemarandrade@gmail.com>
2023-04-19 16:50:03 -0400

    feat: improve reponsive to SHowPaginateItems component

--- src/components/ShowPaginatedItems.tsx
+++ src/components/ShowPaginatedItems.tsx
@@ -1,4 +1,10 @@
-import { Grid, Pagination } from "@mui/material";
+import {
+  Grid,
+  Pagination,
+  Theme,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { ReactNode } from "react";
 
 interface IShowPaginatedItems {
@@ -11,19 +17,25 @@ const ShowPaginatedItems = ({
   count,
   children,
   onChange,
-}: IShowPaginatedItems) => (
-  <Grid container justifyContent="center">
-    {children}
-    <Grid
-      component={Pagination}
-      count={count}
-      color="primary"
-      justifyContent="center"
-      onChange={(event: React.ChangeEvent<unknown>, page: number) =>
-        onChange(page)
-      }
-    />
-  </Grid>
-);
+}: IShowPaginatedItems) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <Grid container justifyContent="center">
+      {children}
+      <Grid
+        component={Pagination}
+        count={count}
+        color="primary"
+        justifyContent="center"
+        onChange={(event: React.ChangeEvent<unknown>, page: number) =>
+          onChange(page)
+        }
+        siblingCount={isMobile ? 0 : 1}
+      />
+    </Grid>
+  );
+};
 
 export default ShowPaginatedItems;
