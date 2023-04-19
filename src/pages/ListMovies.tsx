164717410660fa82fcfde8181b98fe6e91fd314e
Ilemar <ilemarandrade@gmail.com>
2023-04-19 16:48:56 -0400

    feat: add loading to ListMovies page

--- src/pages/ListMovies.tsx
+++ src/pages/ListMovies.tsx
@@ -1,12 +1,15 @@
 import { Fragment, useMemo } from "react";
 import MoviePoster from "../components/MoviePoster";
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import ShowPaginatedItems from "../components/ShowPaginatedItems";
 import MainLayout from "../layout/MainLayout";
 import { useMoviesProviderState } from "../contexts/MoviesContext";
 import listComponents from "../constants/listComponents";
+import Loading from "../components/Loading";
 
 const ListMovies = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const { movies, totalPages, getMovies, isLoading } = useMoviesProviderState();
 
   const pelis = useMemo(
@@ -16,7 +19,7 @@ const ListMovies = () => {
             return (
               <Fragment key={movie.id}>
                 {movie.poster_path && (
-                  <Grid item xs={12} sm={4} md={3} lg={2} key={movie.id}>
+                  <Grid item xs={4} sm={4} md={3} lg={2} key={movie.id}>
                     <MoviePoster movie={movie} />
                   </Grid>
                 )}
@@ -33,9 +36,9 @@ const ListMovies = () => {
 
   return (
     <MainLayout title={listComponents.showPaginateItems.title}>
-      {isLoading && <div>Loading</div>}
+      <Loading open={isLoading} />
       <ShowPaginatedItems count={totalPages} onChange={handlePages}>
-        <Grid spacing={4} container>
+        <Grid spacing={isMobile ? 2 : 4} container>
           {pelis}
         </Grid>
       </ShowPaginatedItems>
