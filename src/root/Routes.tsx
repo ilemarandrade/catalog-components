d3aeda5e3503a6a917f4ffeb3f0c32e314928a05
Ilemar <ilemarandrade@gmail.com>
2023-04-19 17:50:38 -0400

    feat: add lazy to import routes

--- src/root/Routes.tsx
+++ src/root/Routes.tsx
@@ -1,32 +1,41 @@
-import ListMovies from "../pages/ListMovies";
 import { Redirect, Route } from "react-router-dom";
 import MoviesProvider from "../providers/MovieProviders";
-import ListMoviesSortables from "../pages/ListMoviesSortable";
-import UploadFile from "../pages/UploadFile";
-import ShowComponentToDate from "../pages/ShowComponentToDates";
-import ShowComponentBarChart from "../pages/ShowComponentBarChart";
 import listComponents from "../constants/listComponents";
+import { Suspense, lazy } from "react";
+import Loading from "../components/Loading";
+
+const ListMovies = lazy(() => import("../pages/ListMovies"));
+const ListMoviesSortables = lazy(() => import("../pages/ListMoviesSortable"));
+const UploadFile = lazy(() => import("../pages/UploadFile"));
+const ShowComponentToDates = lazy(
+  () => import("../pages/ShowComponentToDates")
+);
+const ShowComponentBarChart = lazy(
+  () => import("../pages/ShowComponentBarChart")
+);
 
 const Routes = () => (
   <section>
-    <MoviesProvider>
-      <Route path={listComponents.showPaginateItems.path}>
-        <ListMovies />
+    <Suspense fallback={<Loading open />}>
+      <MoviesProvider>
+        <Route path={listComponents.showPaginateItems.path}>
+          <ListMovies />
+        </Route>
+        <Route path={listComponents.sortableList.path}>
+          <ListMoviesSortables />
+        </Route>
+      </MoviesProvider>
+      <Route path={listComponents.uploadFile.path}>
+        <UploadFile />
       </Route>
-      <Route path={listComponents.sortableList.path}>
-        <ListMoviesSortables />
+      <Route path={listComponents.datePicker.path}>
+        <ShowComponentToDates />
       </Route>
-    </MoviesProvider>
-    <Route path={listComponents.uploadFile.path}>
-      <UploadFile />
-    </Route>
-    <Route path={listComponents.datePicker.path}>
-      <ShowComponentToDate />
-    </Route>
-    <Route path={listComponents.barChart.path}>
-      <ShowComponentBarChart />
-    </Route>
-    <Redirect exact path="/" to={listComponents.showPaginateItems.path} />
+      <Route path={listComponents.barChart.path}>
+        <ShowComponentBarChart />
+      </Route>
+      <Redirect exact path="/" to={listComponents.showPaginateItems.path} />
+    </Suspense>
   </section>
 );
 
