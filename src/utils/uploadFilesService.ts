d887500d881f7f5b1c3f89f069c7a2b96e5caa9f
Ilemar <ilemarandrade@gmail.com>
2023-04-19 11:54:14 -0400

    feat: add uploadFilesServices class to upload file

---
+++ src/utils/uploadFilesService.ts
@@ -0,0 +1,22 @@
+import { AxiosProgressEvent } from "axios";
+import axiosInstance from "./axiosInstances";
+
+class UploadFilesService {
+  upload(
+    file: File,
+    onUploadProgress: (progressEvent: AxiosProgressEvent) => void
+  ): Promise<any> {
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    return axiosInstance.post("/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress,
+    });
+  }
+}
+
+export default new UploadFilesService();
