84dc4b7cec2cd1bc5bdebc440e972a7bd0bdb1da
Ilemar <ilemarandrade@gmail.com>
2023-04-19 22:22:08 -0400

    feat: improve of bar chart

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import {
   BarChart,
   Bar,
@@ -9,13 +10,41 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+const CustomTooltip = ({ active, payload, label, legendName }: any) => {
+  if (active && payload && payload.length) {
+    const isTotalSales = payload[0].name === "total_sales";
+    return (
+      <Box
+        sx={{
+          background: (theme) => theme.palette.grey[100],
+          p: 1,
+          borderRadius: 2,
+          minWidth: 150,
+          borderColor: (theme) => theme.palette.grey[500],
+          borderWidth: 1,
+          borderStyle: "solid",
+        }}
+      >
+        <Typography>{label}</Typography>
+        <Typography
+          sx={{ color: payload[0].fill, textTransform: "capitalize" }}
+        >{`${legendName}: ${payload[0].value}${
+          isTotalSales ? "$" : ""
+        } `}</Typography>
+      </Box>
+    );
+  }
+
+  return null;
+};
 interface IBarChart {
   data: any;
-  dataKey: string;
+  dataKeyY: string;
   fill: string;
+  legendName: string;
 }
 
-const BarChartComponent = ({ data, dataKey, fill }: IBarChart) => {
+const BarChartComponent = ({ data, dataKeyY, fill, legendName }: IBarChart) => {
   return (
     <ResponsiveContainer
       width="100%"
@@ -27,9 +56,12 @@ const BarChartComponent = ({ data, dataKey, fill }: IBarChart) => {
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey={dataKey} fill={fill} />
+        <Tooltip content={<CustomTooltip legendName={legendName} />} />
+        <Legend
+          wrapperStyle={{ color: fill, fill: fill, fontWeight: 500 }}
+          payload={[{ value: legendName.toUpperCase(), type: "square" }]}
+        />
+        <Bar dataKey={dataKeyY} fill={fill} />
       </BarChart>
     </ResponsiveContainer>
   );
