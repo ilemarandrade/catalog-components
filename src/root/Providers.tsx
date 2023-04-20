4e3ae3485ebd2399a6428acbc2c611e043f7e01a
Ilemar <ilemarandrade@gmail.com>
2023-04-19 20:43:05 -0400

    feat: add theme of material ui

--- src/root/Providers.tsx
+++ src/root/Providers.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from "@mui/material";
 import { ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme";
 
 interface IProviders {
   children: ReactNode;
 }
 
 const Providers = ({ children }: IProviders) => (
-  <BrowserRouter>{children}</BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>{children}</BrowserRouter>
+  </ThemeProvider>
 );
 
 export default Providers;
